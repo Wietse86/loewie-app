fdffe461204313d94523a02d23a01d8026d0335e
Wietse86 <wietsehendriks@hotmail.com>
2026-04-21 20:44:22 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'loewie-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
